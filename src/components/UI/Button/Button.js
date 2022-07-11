d3a3cabd975f0fa6f6a99fc81a9a5dba89554cca
zenoksi4 <zenoken4ik@gmail.com>
2022-07-11 20:27:15 +0300

    initial

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,37 @@
+// import styled from "styled-components";
+import styles from './Button.module.css'
+
+// tagged tamplate leterals = гулить
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   color: white;
+//   background: #00358b;
+//   border-radius: 4px;
+//   box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
+  
+//   @media (min-width: 700px)  {
+//     width: auto;
+//   }
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #245fbd;
+//     box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
+//   }
+// `;
+
+function Button (props) {
+  return (
+    <button type={props.type} className={styles.button}>
+      {props.children}
+    </button>
+  );
+}
+
+export default Button;
